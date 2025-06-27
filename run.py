e55cad523c10cca78db23bc190a4f4c229abf792
Samrat <yashsamrat10080@gmail.com>
2025-06-27 19:46:09 +0530

    Refactor startup and environment

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
 import asyncio
-from OxygenMusic.__main__ import init
+import OxygenMusic
+from OxygenMusic.bootstrap import bootstrap
+
 
 if __name__ == "__main__":
+    (
+        OxygenMusic.app,
+        OxygenMusic.userbot,
+        OxygenMusic.Apple,
+        OxygenMusic.Carbon,
+        OxygenMusic.SoundCloud,
+        OxygenMusic.Spotify,
+        OxygenMusic.Resso,
+        OxygenMusic.YouTube,
+        OxygenMusic.Telegram,
+    ) = bootstrap()
+
+    from OxygenMusic.__main__ import init
+
     asyncio.run(init())
